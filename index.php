b333d3db6b240508c352a39fa25079d9caec6ff7
Thomas Karchesy <tkarchesyspam@gmail.com>
2016-01-25 20:29:27 -0800

    Added MenuItem  Class
    Added MenuItem.php that contains MenuItem class.
    Change to index.php with bare outline of a postback php form.

--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <?php
-include "config.php";
-include "header.php";
-?>
+//index.php
 
-<h1>Aunt Betty's Bisquet Truck</h1>
-<p>Made with the freshest flour and lard.</p>
+include config.php;
+include functions.php;
+include header.php;
 
+//Aunt Betty's Bisquet Truck
 
+define('THIS_PAGE', basename($_SERVER['PHP_SELF']));
+       
+if (isset($_POST['submit'])) {//data submitted
+   
+   //process data and show form again.
+   
+}else{//show form
+   
+   //show virgin form
+   
+}
 
-<?php
-include "footer.php";
-?>
+include footer.php;
