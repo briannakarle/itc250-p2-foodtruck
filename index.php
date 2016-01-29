b156cbc779f6451e9384a6fd203b71bf7e1828b7
Thomas Karchesy <tkarchesyspam@gmail.com>
2016-01-29 02:06:05 -0800

    fixed objects and foreach
    objects can't be created in a function.
    figured out how to do foreach for objects.

--- index.php
+++ index.php
@@ -6,16 +6,20 @@ define('THIS_PAGE',basename($_SERVER['PHP_SELF']));
 include 'MenuItem.php'; // for $MenuItemS
 include 'functions.php';
 // instantiate the menu objects 
-// makeObject() ;
+	$MenuItemS[] = new MenuItem('Willie Biscuit', 'This one will leave you better than stoned', 5.95, 0);
+	$MenuItemS[] = new MenuItem('True Grit Biscuit', 'The one that John Wayne made famous', 7.95, 0);
+	$MenuItemS[] = new MenuItem('Dolly Biscuit', 'Double heapins of good lovin food', 8.88, 0);	   
 
 if(isset($_POST['submit'])) // check to see if this is a new presence or a submitted form
 {// case = submitted form
-showForm();
+
+    showForm($MenuItemS);
 echo ' <pre>';
 var_dump($_POST);
 echo '<pre>';
 }else{//case = new presence
- showForm();
+
+    showForm($MenuItemS);
 echo ' <pre>';
 var_dump($_POST);
 echo '<pre>';
