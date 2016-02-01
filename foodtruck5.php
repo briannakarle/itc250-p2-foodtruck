f7cfcbc3db51d4f7835addea54c80aa98759caa6
briannakarle <brianna.risard@gmail.com>
2016-02-01 01:12:42 -0800

    Styles
    It is blue

--- foodtruck5.php
+++ foodtruck5.php
@@ -21,6 +21,80 @@ $myItem[] = new Item('True Grit Biscuit', 'The one that John Wayne made famous',
 $myItem[] = new Item('Dolly Biscuit', 'Double heapins of good lovin food', 8.88, 0);
 $myItem[] = new Item('Good, Bad, and Ugly', 'Poached egg, fried egg and scrabbled egg', 9.25, 0);
 
+echo '
+  <html>
+  <head>
+    <title> Aunt Betty\'s Biscuit Truck</title>
+    <meta charset="UTF-8">
+    <style>
+        body {
+            font-size: 100%;
+            font-family: arial, sans-serif;
+            line-height: 150%;
+            width:80%;
+            margin:1.5em auto;
+        }
+        
+        h1, h2 {
+            width:100%;
+            text-align:center;
+        }
+        
+        h1 {
+            color: #1a3554;
+        
+        }
+        
+        h2 {
+            color: #356dad;
+            font-size: 1.15em;
+        }
+        
+        p.thanks {
+            border: 2px solid #ccc;
+            border-radius: 8px;
+            box-sizing: border-box;
+            font-size: 1.5em;
+            padding: 50px;
+            text-align: center;
+            width: 100%;
+            
+        }
+        table{
+            border: 2px solid #ccc;
+            border-collapse: collapse;
+            margin: 1em auto;
+        }
+
+        th {
+            background: #1a3554 none repeat scroll 0 0;
+            color: #fff;
+            font-size: 1.25em;
+            font-style: normal;
+            font-weight: normal;
+            letter-spacing: 1px;
+        }
+        td, th {
+            border: 1px solid #eee;
+            padding: 7px 10px;
+        }
+        input.button {
+            background-color: #356dad;
+            border: medium none;
+            border-radius: 8px;
+            color: #fff;
+            font-size: 1em;
+            padding: 10px;
+            text-transform: uppercase;
+            width: 100%;
+        }
+    </style>
+   </head>
+   <body>
+    <h1>  Aunt Betty\'s Biscuit Truck</h1>
+    <h2>Made with the freshest flour and lard</h2>
+    ';
+
 if(isset($_POST['submit']))
 {//data submitted
    /* echo '<pre>';
@@ -29,33 +103,19 @@ if(isset($_POST['submit']))
     echo '</pre>';
     */
     echo '
-  <html>
-  <head>
-    <title> Aunt Betty\'s Biscuit Truck</title>
-    <meta charset="UTF-8">
-    <style>
-        table, th, td {
-            border: 1px solid #ccc;
-        }
-        th {
-            background: #ddd;
-        }
-    </style>
-   </head>
-<body>
-    <h1>  Aunt Betty\'s Biscuit Truck</h1>
-    <h2>Made with the freshest flour and lard</h2>
+
     <form action="" method="post">
          <table>
-             <tr>
-                 <th>QTY</th>
-                 <th>Biscuit</th>
-                 <th>Description</th>
-                 <th>Price</th>
-                 <th>Total</th>
-             </tr>
-             <tr>
-    
+             <tbody>
+                 <tr>
+                     <th>QTY</th>
+                     <th>Biscuit</th>
+                     <th>Description</th>
+                     <th>Price</th>
+                     <th>Total</th>
+                 </tr>
+                 <tr>
+
 ';
     
 $total = 0;
@@ -63,65 +123,53 @@ $total = 0;
 $max = sizeof($myItem);
     
 for ($x = 0; $x < $max; $x++) {
-                echo '<td>' .  $_POST["quantity" . (string)$x] . '</td>';
-                      
-                echo '<td> ' . $myItem[$x]->name . '</td>'; //item name
-                echo ' <td> ' . $myItem[$x]->description . '</td>';  //item description
-                echo '<td> ' . $myItem[$x]->price . '</td>';  //item price
-                $subtotal = $subtotal + $myItem[$x]->price * $_POST["quantity" . (string)$x];
-                $tax = $subtotal * .096;// * $_POST["quantity" . (string)$x];
-                $total = $subtotal + $tax;
-                setlocale(LC_MONETARY,"en_US");
-                echo '<td> ' . $myItem[$x]->price * $_POST["quantity" . (string)$x] . '</td>' ;//price * quantity
-                echo '</tr>';
-                };
-                echo '<tr>
-                        <td colspan="3"></td>
-                        ' . money_format("<td>Subtotal</td><td> %n", $subtotal) . '</td>
-                    </tr>	
-				    <tr>
-                        <td colspan="3"></td>
-                        ' . money_format("<td>Tax</td><td> %n", $tax) . '</td>
-                    </tr>	
-				    <tr>
-                        <td colspan="3"></td>
-                        ' . money_format("<td>Tax</td><td> %n", $total) . '</td>
-                    </tr>	
-				    <tr>
-                        <td colspan="3"></td>
-                        <td colspan="2"><input id="orderform" type = "submit" name = "order" value="Place your Order" /></td>
+    
+    echo '<td>' .  $_POST["quantity" . (string)$x] . '</td>';
+
+    echo '<td> ' . $myItem[$x]->name . '</td>'; //item name
+    echo ' <td> ' . $myItem[$x]->description . '</td>';  //item description
+    echo '<td> ' . $myItem[$x]->price . '</td>';  //item price
+    $subtotal = $subtotal + $myItem[$x]->price * $_POST["quantity" . (string)$x];
+    $tax = $subtotal * .096;// * $_POST["quantity" . (string)$x];
+    $total = $subtotal + $tax;
+    setlocale(LC_MONETARY,"en_US");
+    echo '<td> ' . $myItem[$x]->price * $_POST["quantity" . (string)$x] . '</td>' ;//price * quantity
+    echo '</tr>';
+    };
+    echo '
+                    <tr>
                     </tr>
                     <tr>
                         <td colspan="3"></td>
-                        <td colspan="2"><input id="orderform" type = "submit" name = "menu" value="Adust Order" /></td>
-                    </tr>';
-                
-
+                        ' . money_format("<td>Subtotal</td><td> %n", $subtotal) . '</td>
+                    </tr>	
+                    <tr>
+                        <td colspan="3"></td>
+                        ' . money_format("<td>Tax</td><td> %n", $tax) . '</td>
+                    </tr>	
+                    <tr>
+                        <td colspan="3"></td>
+                        ' . money_format("<td>Tax</td><td> %n", $total) . '</td>
+                    </tr>	
+                    <tr>
+                        <td colspan="5"><input id="orderform" type = "submit" name = "order" value="Place your Order" class="button" /></td>
+                    </tr>
+                    <tr>
+                        <td colspan="5"><input id="orderform" type = "submit" name = "menu" value="Adust Order" class="button" /></td>
+                    </tr>
+                </tbody>
+            </table>
+        </form>
+    ';
     
 }elseif (isset($_POST['order'])) {
-    echo ' Thanks for your order!';
+    echo '<p class="thanks">Thanks for your order!</p>';
 }
 
 
 
 else{//show form
    echo '
-  <html>
-  <head>
-    <title> Aunt Betty\'s Biscuit Truck</title>
-    <meta charset="UTF-8">
-    <style>
-        table, th, td {
-            border: 1px solid #ccc;
-        }
-        th {
-            background: #ddd;
-        }
-    </style>
-   </head>
-<body>
-    <h1>  Aunt Betty\'s Biscuit Truck</h1>
-    <h2>Made with the freshest flour and lard</h2>
     <form action="" method="post">
          <table>
              <tr>
@@ -143,14 +191,20 @@ $max = sizeof($myItem);
                 echo '<td> ' . $myItem[$x]->price . '</td></tr>';  //item price
                 /*echo '<td> ' . $myItem[$x]->price * $_POST["quantity" . (string)$x]  . '</td></tr>' ; //price * quantity */
                                 }; 
-     
-
-      echo '</table>';
-      echo '  <input id="orderform" type = "submit" name = "submit" value="Calculate Price" />
+    
+      echo '  
+            <tr>
+                <td colspan="5"><input id="orderform" type = "submit" name = "submit" value="Calculate Price" class="button"/></td>
+            <tr>
+        </table>
     </form>
-</body>
-</html>
-'; //submit form
+    ';
+ //submit form
 /* }; */
 };
+
+echo '
+</body>
+</html>
+';
 
